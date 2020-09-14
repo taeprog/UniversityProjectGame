6d70d381c6a8ce8afc55c318ba18b75e6b18635f
taeprog <tae.prog@gmail.com>
2020-09-14 17:16:56 +0300

    Pause menu opens on esc press and some onGame ui

---
+++ Assets/Scenes/scripts/ui/Pause.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPused = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPused)
+            {
+                resume();
+            }
+            else {
+                pause();
+            }
+        }
+    }
+
+    public void pause() {
+        pauseMenu.SetActive(true);
+        isPused = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void resume() {
+        pauseMenu.SetActive(false);
+        isPused = false;
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scenes/scripts/ui/PauseEventsListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseEventsListener : MonoBehaviour
+{
+    public Button resumeBtn;
+    public Button exitBtn;
+
+    public Pause pause;
+
+    void Start()
+    {
+        resumeBtn.onClick.AddListener(onResumeClick);
+        exitBtn.onClick.AddListener(onExitClick);
+    }
+
+    private void onResumeClick() {
+        pause.resume();
+    }
+    private void onExitClick() {
+        Debug.Log("Exit");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+}
